5d03fc784b8f2f4a58499ed9d6434066c2aaa702
Laurent Castañeda <lfcastanedar@unal.edu.co>
2025-04-01 04:24:05 -0500

    Update database schema and add test project
    Added new migrations to enhance user security, including constraints on fields like Password, Email, and CardNumber, and introduced new fields for tracking login attempts. Renamed columns in the Transactions table for clarity. Additionally, initialized a new test project with necessary dependencies for unit testing.

---
+++ Infraestructure/Data/Migrations/20250331051559_UpdateUserSecurity.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserSecurity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginAttemptDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "LoginAttempts",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "Cards",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastLoginAttemptDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LoginAttempts",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "Cards",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+        }
+    }
+}
---
+++ Infraestructure/Data/Migrations/20250401074700_UpdateTransactionTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTransactionTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FeeRate",
+                table: "Transactions",
+                newName: "UFE");
+
+            migrationBuilder.RenameColumn(
+                name: "Fee",
+                table: "Transactions",
+                newName: "PaymentFee");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UFE",
+                table: "Transactions",
+                newName: "FeeRate");
+
+            migrationBuilder.RenameColumn(
+                name: "PaymentFee",
+                table: "Transactions",
+                newName: "Fee");
+        }
+    }
+}
